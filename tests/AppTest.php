7796e32c1b6c86cb1bf0348c06b924aa1b8e66dc
vauban54 <vauban54@gmail.com>
2022-02-03 15:58:44 +0100

    villageTest

---
+++ tests/AppTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class AppTest extends TestCase {
+
+    public function testAreWorking() {
+        $this->assertEquals(2, 1+1);
+    }
+}
